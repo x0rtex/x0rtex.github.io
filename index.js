1ae0069f27aacdd8a38c8992648e34ce65e31626
Alekss Belavskis <39656613+x0rtex@users.noreply.github.com>
2024-03-13 11:58:07 +0000

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+let map;
+
+async function initMap() {
+  const position = { lat: 56.947, lng: 24.108 };
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 10,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Latvia",
+  });
+}
+
+initMap();
